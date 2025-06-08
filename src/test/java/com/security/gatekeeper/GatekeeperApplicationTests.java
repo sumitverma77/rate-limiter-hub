e9a4d4978831e7cc602c26ef37a72a3a90f727dd
sumitverma77 <987sumitverma@gmail.com>
2025-06-09 01:44:57 +0530

    init: project setup

---
+++ src/test/java/com/security/gatekeeper/GatekeeperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.security.gatekeeper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatekeeperApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
